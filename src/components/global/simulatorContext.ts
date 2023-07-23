4584be0796314a5b45fd8ea0de5f71a73d587020
andresha20 <andreshenao.2021@gmail.com>
2023-07-23 00:43:41 -0500

    23/07/2023 - 00:43

--- src/components/global/simulatorContext.ts
+++ src/components/global/simulatorContext.ts
@@ -2,14 +2,8 @@ export const simulatorContext = () => {
     return {
         selectedType: 1,
         pickerSection: true,
-        switchType(e: Event) {
-            if(e.target !== e.currentTarget) return;
-            if (e.target instanceof Element) {
-                let target = e.target.getAttribute("id");
-                if (target) {
-                    this.selectedType = parseInt(target);
-                }
-            }
+        switchType(e: string) {
+            this.selectedType = parseInt(e);
             this.pickerSection = false;
         },
         switchBack() {
